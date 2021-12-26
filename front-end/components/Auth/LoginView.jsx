2c60db66ac23e4498341d52d54b1cd9b0c7f053f
nathanielw44 <nathaniel.watson8@gmail.com>
2021-12-26 00:34:18 -0500

    rough draft of login screen minus button

--- front-end/components/Auth/LoginView.jsx
+++ front-end/components/Auth/LoginView.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import loginIcon from './static/LoginIcon.png';
-import rainbowButton from './static/RainbowButton.png';
+import TextEntry from '../shared/TextEntry.jsx';
 import './static/AuthStyles.css'
 // Convert to absolute import later
 
@@ -9,20 +9,24 @@ import './static/AuthStyles.css'
  * @returns: [html div] which spans all allotted space
  */
 
-
-function RoundedButton(props) {
-  return (
-    <div className='rectangle'>
-      <h1>this is the rectangle</h1>
-    </div>
-  )
-}
-
-
 class LoginView extends React.Component {
 
   constructor(props) {
     super(props);
+    this.loginUsername = '';
+    this.loginPassword = '';
+  }
+
+  setUsername = (value) => {
+    this.loginUsername = value;
+    // this.setState({ loginUsername: value });
+    console.log("testprop", this.loginUsername);
+  }
+
+  setPassword = (value) => {
+    this.testProp = value;
+    // this.setState({ loginUsername: value });
+    console.log("testprop", this.testProp)
   }
 
   render() {
@@ -32,11 +36,25 @@ class LoginView extends React.Component {
           className='login-icon'
           src={loginIcon}
           alt="Login Icon" />
-        <h2>CTags</h2>
-        <img
-          src={rainbowButton}
-          alt="Login Button" />
-        <RoundedButton />
+        <h2 className='ctag-text'>CTags</h2>
+
+        <div style={{ height: '10vw' }}></div>
+
+        <div className='login-field'>
+          <TextEntry
+            className='login-field'
+            placeholder={'Username'}
+            onChange={this.setUsername}>
+          </TextEntry>
+        </div>
+        <div style={{ height: '5vw' }}></div>
+        <div className='login-field'>
+          <TextEntry
+            className='login-field'
+            placeholder={'Password'}
+            onChange={this.setPassword}>
+          </TextEntry>
+        </div>
 
       </div>
     )
